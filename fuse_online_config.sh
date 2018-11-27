9ab10d930b78e6bd663031fe07ccfb0416d0f30e
Tomas Plevko <tplevko@redhat.com>
2018-11-27 13:40:57 +0100

    rebuild of images

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -2,12 +2,12 @@
 git_fuse_online_install="1.5.8"
 
 # Tags used for the productised images
-tag_server="1.2-4"
-tag_ui="1.2-2"
-tag_meta="1.2-3"
-tag_s2i="1.2-3"
-tag_upgrade="1.2-6"
-tag_operator="1.2-5"
+tag_server="1.2-5"
+tag_ui="1.2-3"
+tag_meta="1.2-5"
+tag_s2i="1.2-5"
+tag_upgrade="1.2-8"
+tag_operator="1.2-7"
 
 # Docker repository for productised images
 repository="fuse7"
